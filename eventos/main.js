00d4062cef8c3bc8e28cf3daeb1d59c33f05cc72
Israel De Castro <fulatino203@gmail.com>
2018-06-10 23:38:15 -0400

    More changes

--- eventos/main.js
+++ eventos/main.js
@@ -1,5 +1,26 @@
-new Vue({
+const vm = new Vue({
 	el: '#app',
 	data: {
+		nuevaTarea: null,
+		tareas: [
+			'Aprender Vue.js',
+			'Aprender ES6',
+			'Públicar algo todos los días'
+		]
+	},
+	methods: {
+		agregarTarea () {
+			// this, Aquí hace referancia a la instancia Vue
+			this.tareas.unshift(this.nuevaTarea);
+			this.nuevaTarea = null;
+		}
 	}
 });
+
+// Vanilla JavaScript
+
+// function agregarTarea(){
+// 	const input = document.querySelector('input[type="text"]');
+// 	vm.tareas.unshift(input.value);
+// 	input.value = '';
+// }
